abfe08803eeaf26ea864955d290f5833022e9c92
joaonetoeng <joaonetu@live.com>
2020-04-26 16:16:31 -0300

    Criar e remover atributo dinamico de objetos do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -11,5 +11,12 @@ if __name__ == '__main__':
     luciano = Pessoa(joao, nome="Luciano")
     for f in luciano.filhos:
         print(f.nome)
+    # criando atributo dinamico .sobrenome de luciano
+    luciano.sobrenome = 'Ramalho'
+    print(luciano.__dict__)
+    print(joao.__dict__)
+    del luciano.sobrenome
+    print(luciano.__dict__)
+    print(joao.__dict__)
 
 
