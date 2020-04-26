10f1ecbdfe2a42fef84049277fe400732ea02f83
joaonetoeng <joaonetu@live.com>
2020-04-26 15:57:51 -0300

    Criado os atributos de instania nome e idade, da classe pessoa.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,18 @@
 class Pessoa:
+    def __init__(self, nome, idade=26):
+        self.idade = idade
+        self.nome = nome
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá {self}'
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+    p1 = Pessoa("Luciano", 40)
+    p2 = Pessoa("João")
+    print(Pessoa.cumprimentar(p1))
+    print(p1.cumprimentar())
+    print(p1.nome, p1.idade)
+    print(p2.nome, p2.idade)
+    p1.nome = 'Renzo'
+    print(p1.nome)
+
+
