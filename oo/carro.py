43a034cb707de12eaf78884e32c948c4a7010c83
joaonetoeng <joaonetu@live.com>
2020-04-27 11:39:02 -0300

    Modificado a classe Carro, corrigindo para a forma que o renzo implementou.

--- oo/carro.py
+++ oo/carro.py
@@ -34,11 +34,21 @@ class Motor(object):
         else:
             self.velocidade -= 2
 
+
+# Declaração das constantes do problema
+NORTE = 'Norte'
+SUL = 'Sul'
+LESTE = 'Leste'
+OESTE = 'Oeste'
+
 class Direcao(object):
+    rotacao_a_direcita_dict = {NORTE:LESTE, LESTE:SUL, SUL:OESTE, OESTE:NORTE }
+
     def __init__(self, valor='Norte'):
         self.valor = valor
 
     def girar_a_esquerda(self):
+        # Como o eu implementei
         if self.valor == 'Norte':
             self.valor = 'Oeste'
         elif self.valor == 'Oeste':
@@ -51,19 +61,12 @@ class Direcao(object):
             print('Indique uma direção válida. Ex.: Norte, Sul, Leste ou Oeste')
 
     def girar_a_direita(self):
-        if self.valor == 'Norte':
-            self.valor = 'Leste'
-        elif self.valor == 'Leste':
-            self.valor = 'Sul'
-        elif self.valor == 'Sul':
-            self.valor = 'Oeste'
-        elif self.valor == 'Oeste':
-            self.valor = 'Norte'
-        else:
-            print('Indique uma direção válida. Ex.: Norte, Sul, Leste ou Oeste')
+        # Como o Renzo implementou
+        self.valor = self.rotacao_a_direcita_dict[self.valor]
 
-'''
 motor = Motor()
 direcao = Direcao()
-carro = Carro(motor, direcao)
-'''
+carro_do_renzo = Carro(motor, direcao)
+carro_do_renzo.girar_a_direita()
+carro_do_renzo.girar_a_direita()
+print(carro_do_renzo.calcular_direcao())
